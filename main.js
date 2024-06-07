c56a0864029c5fb706c33d868c918d372acabe02
Balla Sunil Kumar <87064284+BALLASUNILKUMAR@users.noreply.github.com>
2024-06-07 14:56:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Ethical Hacker", "Jr.Penteration Tester", "SOC Analyst"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
